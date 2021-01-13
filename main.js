e5d94d5b7fce2a9caf733a05b8381185c3f39f76
AxelDemorest <axel.demorest@gmail.com>
2021-01-13 16:04:35 +0100

    Transcription des données terminée (story 2)

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk');
-const fs = require('fs')
+const fs = require('fs');
+const { prependOnceListener } = require('process');
 
 // ------------------- //
 //       STORY 1       //
@@ -18,9 +19,24 @@ if (args[2] !== "-action") {
 if (args[3] === "transform" && args[4] === "./movies.json" && args[5] === "movies.out.json") {
     fs.readFile(args[4], { encoding: 'utf8' }, function (err, data) {
         if (err) return console.error(err);
-        fs.writeFile('movies.out.json', data, function(erreur) {
-            if (erreur) {
-                console.log(erreur)}
-        })
-    });
+
+        let dataParse = JSON.parse(data);
+
+        for (i = 0; i < dataParse.length; i++) {
+
+            var timeStamp = dataParse[i]['release_date'] // le TimeStamp à convertir
+        
+            date = new Date(timeStamp*1000); // pour obtenir le timeStamp en millisecondes
+    
+            annee = date.getFullYear();
+
+            dataParse[i]['title'] = dataParse[i]['title'] + ` (${annee})`;
+
+        }
+
+        fs.writeFile('./movies.out.json', JSON.stringify(dataParse, null, 2), (err) => {
+            if (err) throw err;
+        });
+
+    })
 }
