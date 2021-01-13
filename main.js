adb677f8e3c5b8e177aa90e8120dbf0efd726fac
AxelDemorest <axel.demorest@gmail.com>
2021-01-13 13:34:39 +0100

    Première partie de la transaction des data faite

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk');
+const fs = require('fs')
 
 // ------------------- //
 //       STORY 1       //
@@ -6,7 +7,7 @@ const chalk = require('chalk');
 
 var args = process.argv;
 
-if(args[2] !== "-action") {
+if (args[2] !== "-action") {
     return console.log(chalk.red("[ERROR] - Le premier argument doit toujours être \"action\"."));
 }
 
@@ -14,3 +15,12 @@ if(args[2] !== "-action") {
 //       STORY 2       //
 // ------------------- //
 
+if (args[3] === "transform" && args[4] === "./movies.json" && args[5] === "movies.out.json") {
+    fs.readFile(args[4], { encoding: 'utf8' }, function (err, data) {
+        if (err) return console.error(err);
+        fs.writeFile('movies.out.json', data, function(erreur) {
+            if (erreur) {
+                console.log(erreur)}
+        })
+    });
+}
