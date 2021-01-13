2acef8f2244768fba4b3b8f0de9ee001478de79f
AxelDemorest <axel.demorest@gmail.com>
2021-01-13 12:23:35 +0100

    Ajout de movies.json

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const chalk = require('chalk');
 
+// ------------------- //
+//       STORY 1       //
+// ------------------- //
+
 var args = process.argv;
 
 if(args[2] !== "-action") {
     return console.log(chalk.red("[ERROR] - Le premier argument doit toujours être \"action\"."));
 }
+
+// ------------------- //
+//       STORY 2       //
+// ------------------- //
+
