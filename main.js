9711b66872395992144066d480660cded1ed2397
Axel Demorest <axel@mbp-de-axel.home>
2021-01-14 18:35:57 +0100

    Story 5 non terminée

--- main.js
+++ main.js
@@ -17,6 +17,9 @@ if (args[2] !== "-action") {
 // ------------------- //
 
 if (args[3] === "transform" && args[4] === "./movies.json" && args[5] === "movies.out.json") {
+
+    let start = new Date().getTime();
+
     fs.readFile(args[4], { encoding: 'utf8' }, function (err, data) {
         if (err) return console.error(err);
 
@@ -41,19 +44,21 @@ if (args[3] === "transform" && args[4] === "./movies.json" && args[5] === "movie
         });
 
     })
+
+    let stop = new Date().getTime();
+
+    console.log(chalk.blue("---------------------------"))
+    console.log("Temps d'exécution : " + (stop - start));
+    console.log(chalk.blue("---------------------------"))
 }
 
 // ------------------- //
 //       STORY 3       //
 // ------------------- //
 
-console.log(chalk.blue("---------------------------"))
-
-console.time(chalk.blue("Temps d'exécution"))
-
-console.timeEnd(chalk.blue("Temps d'exécution"))
-
-console.log(chalk.blue("---------------------------"))
+/* let start = new Date().getTime();
+let stop = new Date().getTime();
+console.log("Temps d'exécution : " + (stop - start)); */
 
 // ------------------- //
 //       STORY 4       //
@@ -75,6 +80,9 @@ function sort(tab) {
 }
 
 if (args[3] === "sort_date" && args[4] === "./movies.json" && args[5] === "movies.sort.date.json") {
+
+    let start = new Date().getTime();
+
     fs.readFile('./movies.json', { encoding: 'utf8' }, function (err, data) {
         if (err) return console.error(err);
 
@@ -102,13 +110,91 @@ if (args[3] === "sort_date" && args[4] === "./movies.json" && args[5] === "movie
         });
 
     })
+
+    let stop = new Date().getTime();
+
+    console.log(chalk.blue("---------------------------"))
+    console.log("Temps d'exécution : " + (stop - start));
+    console.log(chalk.blue("---------------------------"))
 }
 
 // ------------------- //
 //       STORY 5       //
 // ------------------- //
 
+function searchBinaire(searchItem, tab, start, end) {
+    let m = start + Math.ceil((end - start) / 2);
 
+    var timeStamp = tab[m]['release_date'] // le TimeStamp à convertir
+
+    date = new Date(timeStamp * 1000); // pour obtenir le timeStamp en millisecondes
+
+    let year = date.getFullYear();
+
+    tab[m].release_date = year;
+
+    console.log(tab[m].release_date)
+
+    console.log("m:" + m)
+
+    if (tab[m].release_date == searchItem) {
+        return tab[m];
+    }
+    if (tab[m].release_date > searchItem) {
+        return searchBinaire(searchItem, tab, start, m - 1);
+    } else {
+        return searchBinaire(searchItem, tab, m + 1, end);
+    }
+}
+
+if (args[3] === "search_date" && args[4] === "./movies.json" && args[5] && args[6]) {
+    fs.readFile('./movies.json', { encoding: 'utf8' }, function (err, data) {
+        if (err) return console.error(err);
+
+        let dataParse = JSON.parse(data);
+
+        let arrayData = [];
+
+        if (args[6] === "true") {
+
+            // Impossible 
+
+        } else if (args[6] === "false") {
+
+            let start = new Date().getTime();
+
+            for (i = 0; i < dataParse.length; i++) {
+
+                var timeStamp = dataParse[i]['release_date'] // le TimeStamp à convertir
+
+                date = new Date(timeStamp * 1000); // pour obtenir le timeStamp en millisecondes
+
+                year = date.getFullYear();
+
+                dataParse[i]['release_date'] = year;
+
+                dataParse[i]['title'] = dataParse[i]['title'] + ` (${year})`;
+
+                if (dataParse[i]['release_date'] == args[5]) {
+
+                    dataParse[i]['release_date'] = timeStamp;
+
+                    arrayData.push(dataParse[i])
+
+                }
+            }
+
+            console.log(arrayData)
+
+            let stop = new Date().getTime();
+
+            console.log(chalk.blue("---------------------------"))
+            console.log("Temps d'exécution : " + (stop - start));
+            console.log(chalk.blue("---------------------------"))
+        }
+
+    })
+}
 
 // ------------------- //
 //       STORY 6       //
@@ -130,6 +216,9 @@ function sortFilm(tab) {
 }
 
 if (args[3] === "search_key_word" && args[4] === "./movies.json" && args[5] && args[6]) {
+
+    let start = new Date().getTime();
+
     fs.readFile('./movies.json', { encoding: 'utf8' }, function (err, data) {
         if (err) return console.error(err);
 
@@ -149,22 +238,22 @@ if (args[3] === "search_key_word" && args[4] === "./movies.json" && args[5] && a
 
             let genresTable = dataParse[i]['genres'];
 
-            if(typeof(genresTable) != 'undefined') {
-            
-            let stringOverviewArray = dataParse[i]['overview']
+            if (typeof (genresTable) != 'undefined') {
 
-            let stringOverviewArraySplit = stringOverviewArray.split(' ');
+                let stringOverviewArray = dataParse[i]['overview']
 
-            if (genresTable.includes(args[6]) && stringOverviewArraySplit.includes(args[5])) {
-                array.push(dataParse[i]);
+                let stringOverviewArraySplit = stringOverviewArray.split(' ');
+
+                if (genresTable.includes(args[6]) && stringOverviewArraySplit.includes(args[5])) {
+                    array.push(dataParse[i]);
+                }
             }
-        }
 
         }
 
         sortFilm(array);
 
-        if(tab.length === 0) {
+        if (tab.length === 0) {
             console.log(chalk.red(`[ERROR] Aucun film n'a été trouvé avec les mots clefs indiqués.`));
         } else {
             console.log(chalk.green("---------------------------------------------------------------------------"))
@@ -173,6 +262,13 @@ if (args[3] === "search_key_word" && args[4] === "./movies.json" && args[5] && a
             console.log(array[1]);
         }
     })
+
+    let stop = new Date().getTime();
+
+    console.log(chalk.blue("---------------------------"))
+    console.log("Temps d'exécution : " + (stop - start));
+    console.log(chalk.blue("---------------------------"))
+
 } else if (!args[5] || !args[6] && (args[3] === "search_key_word" && args[4] === "./movies.json")) {
     console.log(chalk.red(`[ERROR] Il manque un ou des argument(s)`));
 }
